b341a9efc4efd4ce294a9dcbfaecf9e622e58d0f
Zweemanuel <emanueldias16@gmail.com>
2023-04-26 19:51:01 +0200

    Implemented OpenStreetMap map

--- app/src/main/java/com/example/airqualitylux/MainActivity.java
+++ app/src/main/java/com/example/airqualitylux/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.airqualitylux;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize mapView
+        MapView mapView = findViewById(R.id.mapView);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        // Remove default zoom buttons and set multi-touch controls
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
+        mapView.setMultiTouchControls(true);
+        // Set initial view and zoom level
+        mapView.getController().setZoom(10.5);
+        mapView.getController().setCenter(new GeoPoint(49.8153, 6.1296)); // Set a default center point
+
+        // Configure mapView
+        mapView.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getTileProvider().clearTileCache();
     }
 }
